017197d61dcc12d4ec61acb6110c75f213683303
kayleighbisset <BISSETK@my.erau.edu>
2026-03-24 21:09:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 // SE300 Project!!!!!!!!!!!!!!!!!!!!!! IM GOONING RN
-
 #include "Management.hpp"
 #include <iostream>
 #include <cstdlib>
@@ -11,4 +10,37 @@ Manager::Manager() {
     // [SDD_HLD_MGMT_001]
 }
 
+// Management Class
+void start Management::() {
+    // [SDD_HLD_MGMT_002]
+    cout<<"ARATS Program has started."<< endl;
+}
 
+// EventManager
+# include "EventManager.hpp"
+
+EventManager::EventManager() {
+    // [SDD_HLD_EVENT_001]
+}
+
+void EventManager::createEvent(const std::string& name, const std::string& date) {
+    // [SDD_HLD_EVENT_001]
+    events.push_back({name, date});
+}
+
+std::string EventManager::generateQRCode(const Event& event) {
+    // [SDD_HLD_EVENT_002]
+    return qrCodeGenerator.generate(event.name, event.date);
+}
+
+// QRCodeGenerator
+#include "QRCodeGenerator.h"
+
+QRCodeGenerator::QRCodeGenerator() {
+    // [SDD_HLD_QR_001]
+}
+
+std::string QRCodeGenerator::generate(const std::string& eventName, const std::string& eventDate) {
+    // [SDD_HLD_QR_002]
+    return "QR_" + eventName + "_" + eventDate;
+}

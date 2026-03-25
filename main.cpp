6a103993c6f5999b066f06f7113962b946eda1eb
kayytrujillo <kaytruji15m@gmail.com>
2026-03-24 20:55:50 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
-# SE300 Project!!!!!!!!!!!!!!!!!!!!!! IM GOONING RN
-# include "Management.hpp"
-# include <iostream>
+// SE300 Project!!!!!!!!!!!!!!!!!!!!!! IM GOONING RN
+
+#include "Management.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
 
 Manager::Manager() {
     // [SDD_HLD_MGMT_001]
 }
+
+

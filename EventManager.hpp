9825233528c5b8350f0998de809bc9c094eace37
kayytrujillo <kaytruji15m@gmail.com>
2026-04-04 16:20:46 -0700

    Update EventManager.hpp

--- EventManager.hpp
+++ EventManager.hpp
@@ -1,2 +1,22 @@
 #ifndef EVENTMANAGER_HPP
 #define EVENTMANAGER_HPP
+
+#include <vector>
+#include <string>
+#include "QRCodeGenerator.h"
+using namespace std;
+
+class EventManager {
+private:
+    vector<Event> events;
+    QRCodeGenerator qrCodeGenerator;
+
+public:
+    EventManager();
+
+    void createEvent(string title, string date);
+    void displayEvents();
+    vector<Event>& getEvents();
+};
+
+#endif

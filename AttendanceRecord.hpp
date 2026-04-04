cb97b6d8a58002deca55b8edc22245a5733b8ae4
kayytrujillo <kaytruji15m@gmail.com>
2026-04-04 15:56:30 -0700

    Update AttendanceRecord.hpp

--- AttendanceRecord.hpp
+++ AttendanceRecord.hpp
@@ -1,1 +1,27 @@
+#ifndef ATTENDANCERECORD_HPP
+#define ATTENDANCERECORD_HPP
 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class AttendanceRecord {
+private:
+  string attendeeName;
+  string attendeeEmail;
+  string eventTitle;
+  string eventDate;
+  vector<string> customFields;
+public:
+  AttendanceRecord();
+  AttendanceRecord(string name, string email, string title, string date, vector<string> fields); //default constructor
+
+  //getter functions
+  string getAttendeeName();
+  string getAttendeeEmail();
+  string getEventTitle();
+  string getEventDate();
+  vector<string> getCustomeFields();
+};
+#endif

664d64cc6cf3ec18da40b02c702462ed54d83664
chaiarij <chaia@my.erau.edu>
2026-04-16 21:25:29 -0700

    File input and output
    .c file and the two text files i've been working with

---
+++ FileIOTest.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+  //Reads in input
+  char date[50], club[50], otherinfo[50][50], menuChoice[50];
+  char timestamps[100][128];
+  char names[100][128];
+  char emails[100][128];
+  char line[128];  //for reading in from file
+  int i=0, j=0, k=0, l=0; //iterators
+  int choice=1; //for while loops
+  
+  printf("\n\nWhat is the date of your meeting? ");
+  fgets(date, sizeof(date), stdin);
+  printf("\n\nWhat club/organization is this for? ");
+  fgets(club, sizeof(club), stdin);
+  
+  printf("\n\nDo you have other relevant meeting information? (1 for No, 2 for Yes): ");
+  fgets(menuChoice, sizeof(menuChoice), stdin);
+  choice = strtol(menuChoice, NULL, 10);
+  while (choice!=1 && choice!= 2) {
+    printf("\n\nInvalid input. Please enter either 1 or 2: ");
+    fgets(menuChoice, sizeof(menuChoice), stdin);
+    choice = strtol(menuChoice, NULL, 10);
+  }
+  
+  if (choice != 1) {
+    printf("\n\nContinue adding items or enter 1 when finished: ");
+    
+    while (choice != 1) {
+      fgets(menuChoice, sizeof(menuChoice), stdin);
+      choice = strtol(menuChoice, NULL, 10);
+      if (choice != 1) {
+        strcpy(otherinfo[i], menuChoice);
+        i++;
+      }
+    }
+  }
+  
+  
+  FILE *AttInput;
+  AttInput = fopen("SampleAttendanceData.csv", "r");
+  if (AttInput == NULL) {
+    printf("Error: Could not open file.\n");
+    return 0;
+  }
+  
+  while (fgets(line, sizeof(line), AttInput) != NULL) {
+    if (sscanf(line, "\"%127[^\"]\",\"%127[^\"]\",\"%127[^\"]\"", timestamps[j], names[j], emails[j]) == 3) {
+      j++; 
+    }
+  }
+  fclose(AttInput);
+
+    
+  //Opens file in write mode
+  FILE *AttForm;
+  AttForm = fopen("FileIOTest.csv", "w");
+  
+  if (AttForm == NULL) {
+    printf("Error: Could not open file.\n");
+    return 0;
+  }
+
+  //Writes to the form
+  for (k = 0; k < j; k++) {
+    fprintf(AttForm, "%s,%s,%s\n", timestamps[k],names[k], emails[k]);
+  }
+  
+  //Writes other info at the bottom
+  fprintf(AttForm, "\n\nDate: %s", date);
+  fprintf(AttForm, "Club: %s", club);
+  
+  if (i>0) {
+    fprintf(AttForm, "\nOther Info: \n");
+    for (l=0; l<i; l++) {
+      fprintf(AttForm, "%s", otherinfo[l]);
+    }
+  }
+  
+  //Closes file
+  fclose(AttForm);
+
+  return(0);
+}

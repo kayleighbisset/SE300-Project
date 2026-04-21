90964d0373b24ae557d058cb943a63148e234529
kayytrujillo <kaytruji15m@gmail.com>
2026-04-21 09:43:18 -0700

    Update AttendanceSystem.hpp

--- AttendanceSystem.hpp
+++ AttendanceSystem.hpp
@@ -1,2 +1,20 @@
 #ifndef ATTENDANCESYSTEM_HPP
 #define ATTENDANCESYSTEM_HPP
+
+#include <string>
+#include <vector>
+#include "AttendanceProcessor.hpp"
+using namespace std;
+
+class AttendanceSystem {
+private:
+    AttendanceProcessor attendanceProcessor;
+
+public:
+    AttendanceSystem();
+
+    void submitAttendance(string name, string email, string eventTitle, string eventDate, vector<string> customFields);
+    AttendanceProcessor& getProcessor();
+};
+
+#endif

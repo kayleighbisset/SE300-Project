50743460984ae13b46706b4e5df8f71074d6d20a
kayytrujillo <kaytruji15m@gmail.com>
2026-04-21 09:43:30 -0700

    Update DuplicateChecker.hpp

--- DuplicateChecker.hpp
+++ DuplicateChecker.hpp
@@ -1,16 +1,16 @@
 #ifndef DUPLICATECHECKER_HPP
 #define DUPLICATECHECKER_HPP
 
-#include vector
 #include <string>
-#include "ATTENDANCESYSTEM_HPP"
+#include <vector>
 using namespace std;
+
+class AttendanceRecord;
 
 class DuplicateChecker {
 public:
-  DuplicateChecker();
-  bool checkDuplicate(vector<AttendanceRecord>) //to be continued
-
-
+    DuplicateChecker();
+    bool checkDuplicate(vector<AttendanceRecord>& records, string email, string eventTitle, string eventDate);
+};
 
 #endif

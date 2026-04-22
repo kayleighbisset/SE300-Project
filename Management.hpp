f45e55b7c919cfe5786329bc9726def7b37e6f95
kayytrujillo <kaytruji15m@gmail.com>
2026-04-21 21:36:32 -0700

    Update Management.hpp

--- Management.hpp
+++ Management.hpp
@@ -1,14 +1,7 @@
 #ifndef MANAGEMENT_HPP
 #define MANAGEMENT_HPP
 
-#include "EventManager.hpp"
-#include "AttendanceSystem.hpp"
-
 class Management {
-private:
-    EventManager eventManager;
-    AttendanceSystem attendanceSystem;
-
 public:
     Management();
     void start();

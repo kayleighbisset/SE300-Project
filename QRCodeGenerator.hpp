464a94108c57d36de3272da16ae2167a3c2f0c64
kayytrujillo <kaytruji15m@gmail.com>
2026-04-21 21:36:46 -0700

    Update QRCodeGenerator.hpp

--- QRCodeGenerator.hpp
+++ QRCodeGenerator.hpp
@@ -1,13 +1,17 @@
 #ifndef QRCODEGENERATOR_HPP
 #define QRCODEGENERATOR_HPP
 
-#include <string>
+#include <cstring>
+#include <iostream>
 using namespace std;
 
 class QRCodeGenerator {
 public:
     QRCodeGenerator();
     string generate(string eventTitle, string eventDate);
+
+    
+    //refer back on how to use API Key with create QR QRCodeGenerator (mention how this was still in progress)
 };
 
 #endif

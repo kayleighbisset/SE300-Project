9b3bc7026081c2f8f30707fbfbefe807e4b28ab6
kayytrujillo <kaytruji15m@gmail.com>
2026-04-21 21:34:42 -0700

    Update AttendanceProcessor.hpp

--- AttendanceProcessor.hpp
+++ AttendanceProcessor.hpp
@@ -1,42 +1,27 @@
-#ifndef ATTENDANCEPROCESSOR_HPP
-#define ATTENDANCEPROCESSOR_HPP
+#ifndef ATTENDANCERECORD_HPP
+#define ATTENDANCERECORD_HPP
 
+#include <iostream>
 #include <string>
 #include <vector>
-#include "DuplicateChecker.hpp"
 using namespace std;
 
 class AttendanceRecord {
 private:
-    string attendeeName;
-    string attendeeEmail;
-    string eventTitle;
-    string eventDate;
-    vector<string> customFields;
-
+  string attendeeName;
+  string attendeeEmail;
+  string eventTitle;
+  string eventDate;
+  vector<string> customFields;
 public:
-    AttendanceRecord();
-    AttendanceRecord(string name, string email, string title, string date, vector<string> fields);
+  AttendanceRecord();
+  AttendanceRecord(string name, string email, string title, string date, vector<string> fields); //default constructor
 
-    string getAttendeeName();
-    string getAttendeeEmail();
-    string getEventTitle();
-    string getEventDate();
-    vector<string> getCustomFields();
+  //getter functions
+  string getAttendeeName();
+  string getAttendeeEmail();
+  string getEventTitle();
+  string getEventDate();
+  vector<string> getCustomFields();
 };
-
-class AttendanceProcessor {
-private:
-    vector<AttendanceRecord> records;
-    DuplicateChecker duplicateChecker;
-
-public:
-    AttendanceProcessor();
-
-    bool validate(string email);
-    bool store(AttendanceRecord record);
-    void exportCSV(string fileName);
-    vector<AttendanceRecord>& getRecords();
-};
-
 #endif

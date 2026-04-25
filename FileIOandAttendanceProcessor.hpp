d73b2c645c015c8005581a2ce84a9b64ab9bdc27
kayytrujillo <kaytruji15m@gmail.com>
2026-04-24 22:29:41 -0700

    Update FileIOandAttendanceProcessor.hpp
    trace tags!

--- FileIOandAttendanceProcessor.hpp
+++ FileIOandAttendanceProcessor.hpp
@@ -1,11 +1,20 @@
 #ifndef FILEIOANDATTENDANCEPROCESSOR_HPP
 #define FILEIOANDATTENDANCEPROCESSOR_HPP
 
-int attendanceProcessor(
-    const char* names[],
-    const char* emails[],
-    int count,
-    int badPositions[]
-);
+#include <stdbool.h>
+
+extern char nameList[100][128];
+extern char emailList[100][128];
+extern char timestamps[100][128];
+extern char names[100][128];
+extern char emails[100][128];
+
+// [SDD_HLD_DUP_001]
+bool duplicateChecker(char list[][128], char *value, int count);
+
+// [SDD_HLD_PRO_001]
+// [SDD_HLD_PRO_002]
+// [SDD_HLD_PRO_003] 
+int attendanceProcessor(int count, int badPositions[]);
 
 #endif

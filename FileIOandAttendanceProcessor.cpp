8a5c18d4f08998867f7828ece1e3e029980ea5d3
kayytrujillo <kaytruji15m@gmail.com>
2026-04-24 22:28:45 -0700

    Update FileIOandAttendanceProcessor.cpp
    trace tags

--- FileIOandAttendanceProcessor.cpp
+++ FileIOandAttendanceProcessor.cpp
@@ -1,15 +1,18 @@
+#include "FileIOandAttendanceProcessor.hpp"
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 
-char processedNames[100][128];
-char processedEmails[100][128];
+char nameList[100][128], emailList[100][128];
+char timestamps[100][128], names[100][128], emails[100][128];
 
-bool duplicateChecker(char list[][128], int count, char value[]) {
-    int i;
+// [SDD_HLD_DUP_001]
+// [SDD_HLD_DUP_002]
+bool duplicateChecker(char list[][128], char *value, int count) {
+    int j = 0;
 
-    for (i = 0; i < count; i++) {
-        if (strcmp(list[i], value) == 0) {
+    for (j = 0; j < count; j++) {
+        if (strcmp(value, list[j]) == 0) {
             return true;
         }
     }
@@ -17,28 +20,35 @@ bool duplicateChecker(char list[][128], int count, char value[]) {
     return false;
 }
 
-int attendanceProcessor(
-    const char* names[],
-    const char* emails[],
-    int count,
-    int badPositions[]
-) {
+int attendanceProcessor(int count, int badPositions[]) {
     int badCount = 0;
-    int goodCount = 0;
-    int i;
+    int goodCount = 1;
+    int i = 0;
+    int start = 0;
+    const char testDomain[20] = "@my.erau.edu";
 
-    char testDomain[] = "@my.erau.edu";
+    if ((strstr(names[0], "Name") == NULL && strstr(names[0], "name") == NULL)) {
+        start = 0;
+        goodCount = 0;
+    }
+    else {
+        strcpy(nameList[0], names[0]);
+        strcpy(emailList[0], emails[0]);
+        badPositions[0] = 0;
+        start = 1;
+        goodCount = 1;
+    }
 
-    for (i = 1; i < count; i++) {
+    for (i = start; i < count; i++) {
         bool valid = true;
 
-        char currentName[128];
-        char currentEmail[128];
+        char currentName[100];
+        char currentEmail[100];
 
         strcpy(currentName, names[i]);
         strcpy(currentEmail, emails[i]);
 
-        if (duplicateChecker(processedNames, goodCount, currentName)) {
+        if (duplicateChecker(nameList, currentName, goodCount)) {
             valid = false;
         }
 
@@ -46,23 +56,24 @@ int attendanceProcessor(
             valid = false;
         }
 
-        if (duplicateChecker(processedEmails, goodCount, currentEmail)) {
+        if (duplicateChecker(emailList, currentEmail, goodCount)) {
             valid = false;
         }
 
         if (valid) {
-            strcpy(processedNames[goodCount], currentName);
-            strcpy(processedEmails[goodCount], currentEmail);
+            strcpy(nameList[goodCount], currentName);
+            strcpy(emailList[goodCount], currentEmail);
 
-            printf("Valid Entry: %s | %s\n", currentName, currentEmail);
+            printf("Name successfully added: %s \n", currentName);
+            printf("Email successfully added: %s \n", currentEmail);
 
+            badPositions[i] = 0;
             goodCount++;
         }
         else {
-            badPositions[badCount] = i;
+            badPositions[i] = 1;
             badCount++;
-
-            printf("Invalid entry at position %d\n", i);
+            printf("Invalid entry at position %d \n", i);
         }
     }
 
